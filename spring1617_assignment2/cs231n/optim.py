3d6918871eaa7e6d9bf1b71578c3dd410b1771f0
Lisong Guo <lisong.guo@me.com>
2018-04-09 11:42:47 +0200

    implement the sgd_momentum optimizer

--- spring1617_assignment2/cs231n/optim.py
+++ spring1617_assignment2/cs231n/optim.py
@@ -65,7 +65,10 @@ def sgd_momentum(w, dw, config=None):
     # TODO: Implement the momentum update formula. Store the updated value in #
     # the next_w variable. You should also use and update the velocity v.     #
     ###########################################################################
-    pass
+    # reference for the formula:
+    #     http://cs231n.github.io/neural-networks-3/#sgd
+    v = config['momentum'] * v - config['learning_rate'] * dw
+    next_w = w + v
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
